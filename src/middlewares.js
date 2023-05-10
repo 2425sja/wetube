0d48f485871a162ee975dbf3abafb7c275f4f848
soojin <2425sja>
2023-05-10 16:16:56 +0900

    로그인 안하면 redirect되게 만들기

--- src/middlewares.js
+++ src/middlewares.js
@@ -5,4 +5,20 @@ export const localsMiddleware = (req, res, next) => {
   next();
 };
 
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
+
 //res.locals는 퍼그와 연결되어서 바로 쓸수 있는 ........?
